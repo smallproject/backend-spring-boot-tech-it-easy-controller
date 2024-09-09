f79056e26b3e21c56e1e30eed140c396a6e4c8b0
smallproject <j.a.villas@live.com.ph>
2024-09-09 13:29:54 +0200

    added television class model

---
+++ src/main/java/nl/novi/spring_boot_tech_it_easy_controller/models/Television.java
@@ -0,0 +1,23 @@
+package nl.novi.spring_boot_tech_it_easy_controller.models;
+
+import java.util.Date;
+
+public class Television {
+    private int id;
+    private String name;
+    private String brand;
+    private double price;
+    private int currentStock;
+    private int sold;
+    private Date localDate;
+    private String type;
+
+    private double height;
+    private double width;
+    private String screenQuality;
+    private String screenType;
+    private Boolean wifi;
+    private Boolean smartTv;
+    private Boolean voiceControl;
+    private Boolean hdr;
+}
